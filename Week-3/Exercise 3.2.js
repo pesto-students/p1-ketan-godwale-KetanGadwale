b2cf859d19b554c961cd4f46bc5121493d6ab185
KetanGadwale <ketanvgadwale@gmail.com>
2022-04-16 10:42:28 +0530

    Exercise 3.2 - Call, Apply and bind completed

--- Week-3/Exercise 3.2.js
+++ Week-3/Exercise 3.2.js
@@ -1,20 +1,25 @@
 //Call, Apply and Bind Exercise
 
-//call
-
-
-//apply
-
-
 //bind
-
-const fn = () => {
-    console.log(this)
-}.bind(globalThis);
-
-function fn1() {
-    console.log(this)
+function add(a, b) {
+    return a + b;
 }
 
-fn();
-fn1();
+const addFive = add.bind(this, 5);
+console.log(addFive(5)); // => 10
+
+//call and apply
+
+const array = [5, 4, 3, 2, 1];
+const [a, b, c, d, e] = array;
+
+function findMinWithCall(a, b, c, d, e) {
+    return Math.min.call(this, a, b, c, d, e);
+}
+
+function findMinWithApply(array) {
+    return Math.min.apply(this, array);
+}
+
+console.log(findMinWithCall(a, b, c, d, e)); // => 1
+console.log(findMinWithApply(array)); // => 1
--- Week-3/Exercise 3.4.js
+++ Week-3/Exercise 3.4.js
@@ -41,6 +41,6 @@ stack.push(10); // => items : [10]
 stack.push(5); // => items : [10, 5]
 stack.pop(); // => 5 // => items : [10]
 stack.items; // => undefined //use console.log for testing
-stack.items = [10, 100, 1000]; //Adds 'items' property to 'stack' object but orginal 'items' remains untouched.
+stack.items = [10, 100, 1000]; //Adds 'items' property to 'stack' object but original 'items' remains untouched.
 console.log(stack.print()); // => items : [10] not [10, 100, 1000]
 
