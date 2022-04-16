7913b3470a582333b7b83b512e70d96bd141d53d
KetanGadwale <ketanvgadwale@gmail.com>
2022-04-16 10:51:44 +0530

    Exercise 3.4 - print function removed

--- Week-3/Exercise 3.4.js
+++ Week-3/Exercise 3.4.js
@@ -30,9 +30,9 @@ function createStack() {
                 return items.pop();
             },
             //for testing only
-            print() {
-                return items;
-            }
+            // print() {
+            //     return items;
+            // }
         }
     })();
 }
@@ -42,5 +42,5 @@ stack.push(5); // => items : [10, 5]
 stack.pop(); // => 5 // => items : [10]
 stack.items; // => undefined //use console.log for testing
 stack.items = [10, 100, 1000]; //Adds 'items' property to 'stack' object but original 'items' remains untouched.
-console.log(stack.print()); // => items : [10] not [10, 100, 1000]
+// console.log(stack.print()); // => items : [10] not [10, 100, 1000]
 
