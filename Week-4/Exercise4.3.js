eaf564235ab40d58a87aeebf98811c64a4e62a33
KetanGadwale <ketanvgadwale@gmail.com>
2022-04-21 18:44:41 +0530

    next return value changed
    next return value changed to second to get 1 as first two numbers.

--- Week-4/Exercise4.3.js
+++ Week-4/Exercise4.3.js
@@ -3,18 +3,19 @@
 const fibonacciSeriesIterator = {
     _first: -1,
     _second: 0,
-    result: 1,
+    _third: 1,
     [Symbol.iterator]() {
         return this;
     },
     next() {
         this._first = this._second;
-        this._second = this.result;
-        this.result = this._first + this._second;
-        return { value: this.result, done: false }
+        this._second = this._third;
+        this._third = this._first + this._second;
+        return { value: this._second, done: false }
     }
 }
 
+console.log(fibonacciSeriesIterator.next().value)
 console.log(fibonacciSeriesIterator.next().value)
 console.log(fibonacciSeriesIterator.next().value)
 console.log(fibonacciSeriesIterator.next().value)
