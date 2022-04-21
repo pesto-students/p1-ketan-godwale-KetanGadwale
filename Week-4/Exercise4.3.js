ac0f59ae16f24eb136cbde73cbd5936f7c0c3cb4
KetanGadwale <ketanvgadwale@gmail.com>
2022-04-21 18:40:40 +0530

    next method - fibonacci logic

--- Week-4/Exercise4.3.js
+++ Week-4/Exercise4.3.js
@@ -1,13 +1,24 @@
 //Implement Fibonacci Series with Iterators
 
 const fibonacciSeriesIterator = {
-    _first: 0,
-    _second: 1,
+    _first: -1,
+    _second: 0,
     result: 1,
     [Symbol.iterator]() {
         return this;
     },
     next() {
-
+        this._first = this._second;
+        this._second = this.result;
+        this.result = this._first + this._second;
+        return { value: this.result, done: false }
     }
 }
+
+console.log(fibonacciSeriesIterator.next().value)
+console.log(fibonacciSeriesIterator.next().value)
+console.log(fibonacciSeriesIterator.next().value)
+console.log(fibonacciSeriesIterator.next().value)
+console.log(fibonacciSeriesIterator.next().value)
+console.log(fibonacciSeriesIterator.next().value)
+console.log(fibonacciSeriesIterator.next().value)
