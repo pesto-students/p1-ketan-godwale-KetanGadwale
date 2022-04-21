b365c005b1660b194af29ac47cb46e0dfb8af98a
KetanGadwale <ketanvgadwale@gmail.com>
2022-04-21 18:34:09 +0530

    initial values setup

--- Week-4/Exercise4.3.js
+++ Week-4/Exercise4.3.js
@@ -1,8 +1,11 @@
 //Implement Fibonacci Series with Iterators
 
 const fibonacciSeriesIterator = {
+    _first: 0,
+    _second: 1,
+    result: 1,
     [Symbol.iterator]() {
-
+        return this;
     },
     next() {
 
