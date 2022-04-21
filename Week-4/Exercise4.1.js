aedccc0d6592223e35230a95337488019a808a6a
KetanGadwale <ketanvgadwale@gmail.com>
2022-04-21 20:25:15 +0530

    Added getNumber function

---
+++ Week-4/Exercise4.1.js
@@ -0,0 +1,8 @@
+//Implement a function named getNumber which generates a random number.
+//If randomNumber is divisible by 5 it will reject the promise else it will resolve the promise. 
+//Let’s also keep the promise resolution/rejection time as a variable.
+
+
+function getNumber() {
+    return Math.floor(Math.random() * 1000);
+}
