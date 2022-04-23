4c4c6d97667a30110d88ffda380892f1b2977c6d
KetanGadwale <ketanvgadwale@gmail.com>
2022-04-23 18:54:19 +0530

    initial setup

--- Week-4/Exercise4.1.js
+++ Week-4/Exercise4.1.js
@@ -2,11 +2,25 @@
 //If randomNumber is divisible by 5 it will reject the promise else it will resolve the promise. 
 //Let’s also keep the promise resolution/rejection time as a variable.
 
-
+//get Random number between 1 to 1000
 function getNumber() {
     return Math.floor(Math.random() * 1000);
 }
 
 
+//Promise polyfill
+function MyPromise(computationFn) {
 
+}
+
+
+//main
+let num = getNumber();
+
+const promise = new MyPromise((resolve, reject) => {
+    if (num % 5 == 0)
+        reject(num);
+    else
+        resolve(num);
+})
 
