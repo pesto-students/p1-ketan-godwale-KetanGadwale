d3132bcd592a3412291d0b3ea3ac3121e50e2b78
KetanGadwale <ketanvgadwale@gmail.com>
2022-05-16 14:24:09 +0530

    5.4 - test cases added

--- Week-5/Exercise-5.4/calculator.test.js
+++ Week-5/Exercise-5.4/calculator.test.js
@@ -1,1 +1,13 @@
-const mathOperations = require('./mathOperations');
+const { sum, diff, product } = require('./mathOperations');
+
+test('sum test - 2 + 2 = 4', () => {
+    expect(sum(2, 2)).toBe(4);
+});
+
+test('diff test - 15 - 7 = 8', () => {
+    expect(diff(15, 7)).toBe(8);
+});
+
+test('product test - 13 * 3 = 39', () => {
+    expect(product(13, 3)).toBe(39);
+});
