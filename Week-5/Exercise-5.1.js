f8a7cbbadbfee01b01dae9090eeed862812c6688
KetanGadwale <ketanvgadwale@gmail.com>
2022-05-16 15:27:14 +0530

    5.1 - parallel function added

--- Week-5/Exercise-5.1.js
+++ Week-5/Exercise-5.1.js
@@ -1,6 +1,7 @@
 function doTask(name, duration) {
     return new Promise((resolve) => {
         setTimeout(() => {
+            resolve(name);
             console.log(`${name} completed in ${duration} ms`);
         }, duration);
     });
@@ -11,3 +12,10 @@ const tasks = [
     doTask('task2', 3000),
     doTask('task3', 2000),
 ];
+
+async function parallel(tasks) {
+    const result = await Promise.all(tasks);
+    console.log(`${result} ran parallelly!`);
+}
+
+parallel(tasks);
