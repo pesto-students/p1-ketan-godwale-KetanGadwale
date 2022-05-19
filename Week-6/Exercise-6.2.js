1c0234c63df84ec4c86a36ce00713ff21df915b1
Ketan Gadwale <ketan@Ketans-MacBook-Air.local>
2022-05-19 12:51:51 +0530

    6.2 - first row print

--- Week-6/Exercise-6.2.js
+++ Week-6/Exercise-6.2.js
@@ -1,16 +1,24 @@
-function spiral2DArray(arr) {
-    let m = arr[0].length;
-    let n = arr.length;
-    let total = m * n;
-    let index = 0;
+function spiral2DArray(arr, i, j, r, c) {
+    let result = [];
+    //indices are out of bound then return result
+    if (i >= r || j >= c) return result;
 
-    while (index < total) {}
+    //prints first row
+    for (let k = j; k < c; k++) {
+        result.push(arr[i][k]);
+    }
+
+    for(let k =i + 1; )
+
 }
 
-let output = spiral2DArray([
+let arr = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
-]);
+];
 
-console.log(output);
+let m = arr.length;
+let n = arr[0].length;
+
+const output = spiral2DArray(arr, 0, 0, m, n);
