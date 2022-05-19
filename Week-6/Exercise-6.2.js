812cdb4c3e035959be6ebadec4bad1a754d836d8
Ketan Gadwale <ketan@Ketans-MacBook-Air.local>
2022-05-19 13:27:58 +0530

    6.2 - final commit

--- Week-6/Exercise-6.2.js
+++ Week-6/Exercise-6.2.js
@@ -1,7 +1,7 @@
+const result = [];
 function spiral2DArray(arr, i, j, r, c) {
-    let result = [];
     //indices are out of bound then return result
-    if (i >= r || j >= c) return result;
+    if (i >= r || j >= c) return;
 
     //print first row
     for (let k = j; k < c; k++) {
@@ -13,17 +13,35 @@ function spiral2DArray(arr, i, j, r, c) {
         result.push(arr[k][c - 1]);
     }
 
-    //for testing
-    console.log(result);
+    //print last row in reverse
+    if (r - 1 != i) {
+        for (let k = c - 2; k >= j; k--) {
+            result.push(arr[r - 1][k]);
+        }
+    }
+
+    //print first column in reverse
+    if (c - 1 != j) {
+        for (let k = r - 2; k > i; k--) {
+            result.push(arr[k][j]);
+        }
+    }
+
+    spiral2DArray(arr, i + 1, j + 1, r - 1, n - 1);
+
+    return result;
 }
 
 let arr = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
+    [10, 11, 12],
 ];
 
 let m = arr.length;
 let n = arr[0].length;
 
 const output = spiral2DArray(arr, 0, 0, m, n);
+
+console.log(output);
