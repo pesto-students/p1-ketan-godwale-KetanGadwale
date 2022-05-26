d0604478e0a91c407679d6c513f8335eef53a911
KetanGadwale <ketanvgadwale@gmail.com>
2022-05-26 18:22:05 +0530

    8.1 - add function added

--- Week-8/Exercise-8.1.js
+++ Week-8/Exercise-8.1.js
@@ -15,10 +15,36 @@ class BinaryTree {
     addRoot(val) {
         this.root = new Node(val);
     }
+
+    add(val) {
+        let temp = this.root;
+        let newNode = new Node(val);
+        while (true) {
+            if (val < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+
+    maxDepth() {}
 }
 
 const bTree = new BinaryTree();
 
 bTree.addRoot(14);
 
+bTree.add(8);
+bTree.add(27);
+bTree.add(3);
+bTree.add(12);
 console.log(bTree);
