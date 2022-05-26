bbc272a91a86c51048a18638760a5dc1e5614655
KetanGadwale <ketanvgadwale@gmail.com>
2022-05-26 15:31:31 +0530

    7.6 - enqueue & dequeue added

--- Week-7/Exercise-7.6.js
+++ Week-7/Exercise-7.6.js
@@ -6,12 +6,25 @@ class Queue {
         this.rearStack = [];
     }
 
-    enQueue(item) {}
+    enQueue(item) {
+        this.rearStack.push(item);
+    }
 
-    deQueue(item) {}
+    deQueue(item) {
+        if (this.rearStack.length == 0 && this.frontStack.length == 0) {
+            throw Error('Queue - underflow');
+        } else if (this.frontStack.length == 0 && this.rearStack.length > 0) {
+            while (this.rearStack.length != 0) {
+                this.frontStack.push(this.rearStack.pop());
+            }
+            return this.frontStack.pop();
+        } else {
+            return this.frontStack.pop();
+        }
+    }
 
     print() {
-        console.log([...this.frontStack, ...this.rearStack]);
+        console.log([...this.frontStack.reverse(), ...this.rearStack]);
     }
 }
 
@@ -20,6 +33,6 @@ const queue = new Queue();
 queue.enQueue(1);
 queue.enQueue(2);
 queue.enQueue(3);
-queue.deQueue();
+console.log(queue.deQueue());
 
 queue.print();
