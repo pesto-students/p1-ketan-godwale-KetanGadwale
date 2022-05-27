2ec733dea2b5c9035c61b534ea0645601c6beb4d
KetanGadwale <ketanvgadwale@gmail.com>
2022-05-27 12:50:58 +0530

    8.2 - isBinaryTree added

---
+++ Week-8/Exercise-8.2.js
@@ -0,0 +1,65 @@
+//Validate a Binary Tree
+
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    addRoot(val) {
+        this.root = new Node(val);
+        return this.root;
+    }
+
+    addNode(val) {
+        let temp = this.root;
+        let newNode = new Node(val);
+        while (true) {
+            if (val < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return newNode;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return newNode;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+
+    static isBinaryTree(node) {
+        let prev = null;
+        if (node != null) {
+            if (!Tree.isBinaryTree(node.left)) {
+                return false;
+            }
+            if (prev != null && node.value <= prev.value) {
+                return false;
+            }
+            prev = node;
+            return Tree.isBinaryTree(node.right);
+        }
+        return true;
+    }
+}
+
+let tree = new Tree();
+
+let root = tree.addRoot(5);
+tree.addNode(1);
+tree.addNode(6);
+// tree.addNode(4);
+tree.addNode(0);
+
+console.log(Tree.isBinaryTree(root));
