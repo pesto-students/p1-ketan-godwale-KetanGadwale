2c109838f044fa5031c4a72576a3807df5abaf69
KetanGadwale <ketanvgadwale@gmail.com>
2022-05-30 16:43:12 +0530

    8.5 - findTownJudge and whetherAllTrust added

--- Week-8/Exercise-8.5.js
+++ Week-8/Exercise-8.5.js
@@ -9,12 +9,53 @@ class TownGraph {
     }
 
     addTrust(v, w) {
-        this.trustMatrix[v].push(w);
+        this.trustMatrix[v - 1].push(w - 1);
     }
 
     printTrustMatrix() {
         console.log(this.trustMatrix);
     }
 
-    findTownJudge() {}
+    findTownJudge() {
+        let checkList = [];
+        let n = this.trustMatrix.length;
+        for (let i = 0; i < n; i++) {
+            if (this.trustMatrix[i].length == 0) {
+                checkList.push(i);
+            }
+        }
+        if (checkList.length > 0) {
+            while (checkList.length != 0) {
+                let contender = checkList.shift();
+                if (this.whetherAllTrust(contender + 1)) {
+                    return contender + 1;
+                }
+            }
+        } else return -1;
+    }
+
+    whetherAllTrust(j) {
+        for (let i = 0; i < this.trustMatrix.length; i++) {
+            if ((i = j - 1)) continue;
+            if (i.includes(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
+const firstTown = new TownGraph(2);
+firstTown.addTrust(1, 2);
+console.log(firstTown.findTownJudge());
+
+const secondTown = new TownGraph(3);
+secondTown.addTrust(1, 3);
+secondTown.addTrust(2, 3);
+console.log(secondTown.findTownJudge());
+
+const thirdTown = new TownGraph(3);
+thirdTown.addTrust(1, 3);
+thirdTown.addTrust(2, 3);
+thirdTown.addTrust(3, 1);
+console.log(thirdTown.findTownJudge());
