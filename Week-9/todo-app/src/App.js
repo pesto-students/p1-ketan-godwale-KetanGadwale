e7ecca485d7b4da0fd8ebb33eba19deb6b0db38d
KetanGadwale <ketanvgadwale@gmail.com>
2022-06-05 15:24:38 +0530

    TodoForm - form inputs & input state added

--- Week-9/todo-app/src/App.js
+++ Week-9/todo-app/src/App.js
@@ -4,6 +4,7 @@ import TodoForm from './components/TodoForm';
 function App() {
     return (
         <div className='App'>
+            <h1>Todo List</h1>
             <TodoForm />
         </div>
     );
--- Week-9/todo-app/src/components/TodoForm.js
+++ Week-9/todo-app/src/components/TodoForm.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function TodoForm() {
-    return <div>TodoForm</div>;
+    const [input, setInput] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setInput('');
+    };
+    return (
+        <form className='todo-form'>
+            <input
+                className='todo-input'
+                placeholder='Add a todo'
+                onChange={(e) => {
+                    setInput(e.target.value);
+                    console.log(input);
+                }}
+            />
+            <button
+                className='todo-button'
+                type='submit'
+                onSubmit={handleSubmit}
+            >
+                Add
+            </button>
+        </form>
+    );
 }
