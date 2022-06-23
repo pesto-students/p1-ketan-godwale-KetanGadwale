9b4f10d1f09fd784a22e87e9f92147a62ffd46d0
KetanGadwale <ketanvgadwale@gmail.com>
2022-06-23 16:10:20 +0530

    8.6 - getPathsByDFS added

--- Week-8/Exercise-8.6.js
+++ Week-8/Exercise-8.6.js
@@ -1,8 +1,12 @@
 //All Path from source to target
-
 class Graph {
+    #visited;
     constructor(n) {
-        this.adjMatrix = new Array(n).fill([]);
+        this.adjMatrix = new Array(n);
+        for (let i = 0; i < n; i++) {
+            this.adjMatrix[i] = [];
+        }
+        this.#visited = new Array(n).fill(0);
     }
 
     addEdge(v, w) {
@@ -13,9 +17,42 @@ class Graph {
         console.log(this.adjMatrix);
     }
 
-    findAllPaths(source, destination) {}
+    getPathsByDFS(source, destination) {
+        let paths = [];
+        let currentPath = [];
+        const DFS = (source, destination) => {
+            if (this.#visited[source] == 1) {
+                return;
+            }
+
+            this.#visited[source] = 1;
+            currentPath.push(source);
+
+            if (source == destination) {
+                paths.push([...currentPath]);
+                this.#visited[source] = 0;
+                currentPath.pop();
+                return;
+            }
+
+            for (let i of this.adjMatrix[source]) {
+                DFS(i, destination);
+            }
+            currentPath.pop();
+            this.#visited[source] = 0;
+        };
+
+        DFS(source, destination);
+        return paths;
+    }
 }
 
 const graph = new Graph(4);
 
-console.log(graph);
+graph.addEdge(0, 1);
+graph.addEdge(0, 2);
+graph.addEdge(1, 3);
+graph.addEdge(2, 3);
+
+graph.printAdjMatrix();
+console.log(graph.getPathsByDFS(0, 3));
