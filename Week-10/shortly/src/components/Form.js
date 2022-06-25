fe80146fa40ac9cfe70909cbf7aba1dc5f61aa53
KetanGadwale <ketanvgadwale@gmail.com>
2022-06-25 13:19:05 +0530

    Short URL list added

--- Week-10/shortly/src/components/Form.js
+++ Week-10/shortly/src/components/Form.js
@@ -1,25 +1,59 @@
 import React, { useState } from 'react';
+import { Form as FormBS, Button, ListGroup } from 'react-bootstrap';
 
 function Form() {
     const [url, setUrl] = useState('');
+    const [list, setList] = useState([]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        addShortURL(url);
         setUrl('');
     };
 
+    const addShortURL = (url) => {
+        let fetchURL = `https://api.shrtco.de/v2/shorten?url=${url}`;
+        fetch(fetchURL)
+            .then((res) => res.json())
+            .then((data) => {
+                setList([
+                    ...list,
+                    { link: url, short_link: data.result.short_link },
+                ]);
+            });
+    };
+
     return (
-        <div className='Form' onSubmit={handleSubmit}>
-            <form>
-                <input
+        <div className='Form'>
+            <FormBS>
+                <FormBS.Control
+                    type='text'
+                    placeholder='Shorten your link'
                     value={url}
                     onChange={(e) => {
                         setUrl(e.target.value);
                     }}
-                    placeholder='Enter an URL'
                 />
-                <button type='submit'>Shorten URL</button>
-            </form>
+                <div className='d-grid gap-2'>
+                    <Button variant='primary' size='lg' onClick={handleSubmit}>
+                        Shorten
+                    </Button>
+                </div>
+                <FormBS.Text muted>
+                    By clicking SHORTEN, you are agreeing to Shortly's Terms of
+                    Service, Privacy Policy, and Acceptable Use Policy
+                </FormBS.Text>
+            </FormBS>
+            <ListGroup>
+                {list.map((item) => (
+                    <ListGroup.Item>
+                        <span>{item.link}</span> ->
+                        <span style={{ color: 'darkorange' }}>
+                            {item.short_link}
+                        </span>
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
         </div>
     );
 }
--- Week-10/shortly/src/routes/Home.jsx
+++ Week-10/shortly/src/routes/Home.jsx
@@ -2,6 +2,11 @@ import Form from '../components/Form';
 function Home() {
     return (
         <div>
+            <h1>Short links, big results</h1>
+            <span>
+                A URL shortener built with powerful tools to help you grow and
+                protect your brand.
+            </span>
             <Form />
         </div>
     );
