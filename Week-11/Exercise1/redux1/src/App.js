3461f801db085315a5b8b92254ae8641a9c450c3
KetanGadwale <ketanvgadwale@gmail.com>
2022-06-28 07:50:01 +0530

    switch functionality added

--- Week-11/Exercise1/redux1/src/App.js
+++ Week-11/Exercise1/redux1/src/App.js
@@ -1,7 +1,17 @@
+import { useState } from 'react';
 function App() {
+    const [light, setLight] = useState(true);
+
+    const switchLight = () => {
+        setLight(!light);
+    };
+
+    const lightedness = light ? 'lit' : 'dark';
+
     return (
-        <div className='App'>
-            <h1>Hello World</h1>
+        <div className={`App ${lightedness}`}>
+            <h1>Light Switch : {lightedness}</h1>
+            <button onClick={switchLight}>Switch</button>
         </div>
     );
 }
