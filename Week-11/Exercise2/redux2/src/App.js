2247d9218e640a38b932dba0f3f7e8b6180392d2
KetanGadwale <ketanvgadwale@gmail.com>
2022-06-28 09:30:36 +0530

    counter reducer, actions & dispatch added

--- Week-11/Exercise2/redux2/src/App.js
+++ Week-11/Exercise2/redux2/src/App.js
@@ -1,9 +1,18 @@
-function App() {
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { mapDispatchToProps, mapStateToProps } from './store/counterDispatch';
+
+function App(props) {
+    console.log(props);
+    const { counter, increment, reset } = props;
     return (
         <div className='App'>
-            <h1>Hello World</h1>
+            <h1>{counter}</h1>
+            <Button onClick={increment}>Increment</Button>
+            <Button onClick={reset}>Reset</Button>
         </div>
     );
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
